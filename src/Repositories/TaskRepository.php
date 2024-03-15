59c5b1ea593f4084aaa5d9c836a15e210fa5b462
feras <feras.altalib@gmail.com>
2024-03-15 16:17:46 +0100

    _

--- src/Repositories/TaskRepository.php
+++ src/Repositories/TaskRepository.php
@@ -28,15 +28,28 @@
 
         public function create($newTask)
         {
-            $request = 'INSERT INTO todo_task (taskTitle, taskDescription, taskDeadline, taskPriority,taskCategory) VALUES (?, ?, ?, ?, ?)';
+            // Inserting the task
+            $request = 'INSERT INTO todo_task (taskTitle, taskDescription, taskDeadline, taskPriority, taskCategory) VALUES (?, ?, ?, ?, ?)';
             $query = $this->getDb()->prepare($request);
-
+        
             $query->execute([
                 $newTask->getTaskTitle(),
                 $newTask->getTaskDescription(),
                 $newTask->getTaskDeadline(),
                 $newTask->getTaskPriority(),
-                $newTask->getTaskCategory()
+                $newTask->getTaskCategory(),
+                 // Assuming you have a method to get the userID of the user who created the task
             ]);
+        
+            // Inserting the category if it doesn't exist
+            $requestCategory = 'INSERT INTO todo_category (taskCategory) SELECT ? WHERE NOT EXISTS (SELECT 1 FROM todo_category WHERE taskCategory = ?)';
+            $queryCategory = $this->getDb()->prepare($requestCategory);
+            $queryCategory->execute([$newTask->getTaskCategory(), $newTask->getTaskCategory()]);
+        
+            // Inserting the priority if it doesn't exist
+            $requestPriority = 'INSERT INTO todo_priority (taskPriority) SELECT ? WHERE NOT EXISTS (SELECT 1 FROM todo_priority WHERE taskPriority = ?)';
+            $queryPriority = $this->getDb()->prepare($requestPriority);
+            $queryPriority->execute([$newTask->getTaskPriority(), $newTask->getTaskPriority()]);
         }
+        
     }
